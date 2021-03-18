9faa21e1bf6dcc3f907e0c4d590c8b9ca66ffd59
Damu2509 <reddivaridamu25091999@gmail.com>
2021-03-18 22:57:23 +0530

    Incomplete profile updation

--- blogging_app/users/views.py
+++ blogging_app/users/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+
 
 def register(request):
     if request.method == "POST":
@@ -19,6 +20,12 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request,'users/profile.html',{'message':'Successfully displayed the profile page '})
+    u_form = UserUpdateForm()
+    p_form = ProfileUpdateForm()
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request,'users/profile.html',context)
 
 
