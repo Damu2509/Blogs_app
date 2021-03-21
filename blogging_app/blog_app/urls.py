9f16325270ce3deb823d6ddbe3bcd16c95eccc29
Damu2509 <reddivaridamu25091999@gmail.com>
2021-03-21 15:10:53 +0530

    Post created on site dynamically for the first time

--- blogging_app/blog_app/urls.py
+++ blogging_app/blog_app/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from . import views
 from django.contrib import admin
-
+from .views import PostListView, PostDetailView , PostCreateView
 
 urlpatterns = [
 
     path('', views.home,name='home'),
     path('about/', views.about,name='blog-about'),
+    path('post/', PostListView.as_view(),name='post'),
+    path('post/<int:pk>/', PostDetailView.as_view(),name='post-detail'),
+    path('post/new/', PostCreateView.as_view(),name='post-create'),
+
+
+
 ]
