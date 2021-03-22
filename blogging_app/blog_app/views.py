b07189757d15ef1c06cd6ee6edaa8435fd12c154
Damu2509 <reddivaridamu25091999@gmail.com>
2021-03-22 23:25:24 +0530

    Started pagination

--- blogging_app/blog_app/views.py
+++ blogging_app/blog_app/views.py
@@ -23,7 +23,7 @@ class PostListView(ListView):
     model =Post
     template_name = 'blog_app/home.html'  # <app> / <model> _ <viewtype> . html
     context_object_name = 'posts'
-    ordering = ['-date_posted']
+    ordering = ['date_posted']
 
 class PostDetailView(DetailView):
     model =Post
